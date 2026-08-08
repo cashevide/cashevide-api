60852b29cfbabf31829ddcda5e6968fca015371a
Noufal <noufalkakdlr@gmail.com>
2026-08-08 14:36:29 +0530

    refactor(invoices): disable credit deduction on invoice creation

--- invoices/signals.py
+++ invoices/signals.py
@@ -6,14 +6,19 @@ from users.models import UserSubscription
 
 from .models import Invoice
 
+ENABLE_CREDIT_DEDUCTION = False
+
 
 @receiver(post_save, sender=Invoice)
 def deduct_credit_on_invoice_creation(sender, instance, created, **kwargs):
 
-    if created:
-        if hasattr(instance.user, "profile"):
-            user_profile = instance.user.profile
-            user_tier = instance.user.tier
-            if user_tier == UserSubscription.Tier.COMMUNITY:
-                user_profile.credit_points = F("credit_points") - 1
-                user_profile.save(update_fields=["credit_points"])
+    if not ENABLE_CREDIT_DEDUCTION or not created:
+        return
+
+    if not hasattr(instance.user, "profile"):
+        return
+
+    if instance.user.tier == UserSubscription.Tier.COMMUNITY:
+        user_profile = instance.user.profile
+        user_profile.credit_points = F("credit_points") - 1
+        user_profile.save(update_fields=["credit_points"])
